63cb83bcbb0e1803b5bcc7d5caffb5f445a50e37
TarakKora <160771959+TarakKora@users.noreply.github.com>
2024-02-21 23:18:54 +0530

    Update and rename IsArraySortedUsingRec to IsArraySortedUsingRec.java

--- IsArraySortedUsingRec.java
+++ IsArraySortedUsingRec.java
@@ -1,5 +1,4 @@
 public class IsArraySortedUsingRec {
-
         public static void main(String[] args) {
             int[] arr = {1, 2, 3, 4, 5, 6, 7};
             System.out.println(sorted(arr, 0));
