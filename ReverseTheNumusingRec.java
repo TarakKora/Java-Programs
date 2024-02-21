a9a439c768c7f374157bd821691226036e2650c3
TarakKora <160771959+TarakKora@users.noreply.github.com>
2024-02-21 23:34:10 +0530

    Update ReverseTheNumusingRec.java

--- ReverseTheNumusingRec.java
+++ ReverseTheNumusingRec.java
@@ -3,7 +3,12 @@ public class ReverseTheNumusingRec {
         rever(3421);
         System.out.println(result);
     }
-    static int result=0;
+    /*
+    If you declare the result variable as a non-static variable inside the rever method, it will become a local variable within that method. As a local
+    variable, its scope will be limited to the rever method, and it won't be accessible outside of that method. Therefore, if you try to print the value of
+    result in the main method after calling rever, it will result in a compilation error, as result will not be recognized in the main method.
+    */
+    static int result=0; 
     static void rever(int num){
         if(num==0){
             return;
